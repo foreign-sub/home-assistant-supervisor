bbda2ac413b563a7e533f053666fb15625e52039
Restyled.io <commits@restyled.io>
2020-01-27 21:48:50 +0000

    Restyled by yapf

--- hassio/services/validate.py
+++ hassio/services/validate.py
@@ -7,11 +7,12 @@ from .const import SERVICE_MYSQL
 from .modules.mqtt import SCHEMA_CONFIG_MQTT
 from .modules.mysql import SCHEMA_CONFIG_MYSQL
 
-
 SCHEMA_SERVICES_CONFIG = vol.Schema(
     {
-        vol.Optional(SERVICE_MQTT, default=dict): schema_or(SCHEMA_CONFIG_MQTT),
-        vol.Optional(SERVICE_MYSQL, default=dict): schema_or(SCHEMA_CONFIG_MYSQL),
+        vol.Optional(SERVICE_MQTT, default=dict):
+        schema_or(SCHEMA_CONFIG_MQTT),
+        vol.Optional(SERVICE_MYSQL, default=dict):
+        schema_or(SCHEMA_CONFIG_MYSQL),
     },
     extra=vol.REMOVE_EXTRA,
 )
