08987eeda292e46674d696244fb1065a0a189d05
Restyled.io <commits@restyled.io>
2020-02-04 16:06:33 +0000

    Restyled by prettier

--- hassio/api/panel/chunk.92a11ac1b80e0d7839d2.js
+++ hassio/api/panel/chunk.92a11ac1b80e0d7839d2.js
@@ -1,13 +1,24 @@
 (self.webpackJsonp = self.webpackJsonp || []).push([
-  [ 2 ], {
-    176 : function(e, r, n) {
+  [2],
+  {
+    176: function(e, r, n) {
       "use strict";
-      n.r(r), n.d(r, "codeMirror", function() { return c }),
-          n.d(r, "codeMirrorCss", function() { return i });
-      var a = n(54), o = n.n(a), s = n(169), t = (n(170), n(171), n(11));
-      o.a.commands.save = function(
-          e) { Object(t.a)(e.getWrapperElement(), "editor-save") };
-      var c = o.a, i = s.a
+      n.r(r),
+        n.d(r, "codeMirror", function() {
+          return c;
+        }),
+        n.d(r, "codeMirrorCss", function() {
+          return i;
+        });
+      var a = n(54),
+        o = n.n(a),
+        s = n(169),
+        t = (n(170), n(171), n(11));
+      o.a.commands.save = function(e) {
+        Object(t.a)(e.getWrapperElement(), "editor-save");
+      };
+      var c = o.a,
+        i = s.a;
     }
   }
 ]);
