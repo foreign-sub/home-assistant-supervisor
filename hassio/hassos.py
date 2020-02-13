be9a86846e242a39696a9c3574383fbae42f323c
Restyled.io <commits@restyled.io>
2020-02-13 17:06:16 +0000

    Restyled by yapf

--- hassio/hassos.py
+++ hassio/hassos.py
@@ -87,7 +87,8 @@ class HassOS(CoreSysAttributes):
         _LOGGER.info("Fetch OTA update from %s", url)
         try:
             timeout = aiohttp.ClientTimeout(total=600)
-            async with self.sys_websession.get(url, timeout=timeout) as request:
+            async with self.sys_websession.get(url,
+                                               timeout=timeout) as request:
                 if request.status != 200:
                     raise HassOSUpdateError()
 
@@ -131,9 +132,8 @@ class HassOS(CoreSysAttributes):
 
         await self.sys_dbus.rauc.update()
 
-        _LOGGER.info(
-            "Detect HassOS %s / BootSlot %s", self.version, self.sys_dbus.rauc.boot_slot
-        )
+        _LOGGER.info("Detect HassOS %s / BootSlot %s", self.version,
+                     self.sys_dbus.rauc.boot_slot)
         with suppress(DockerAPIError):
             await self.instance.attach(tag="latest")
 
@@ -180,7 +180,8 @@ class HassOS(CoreSysAttributes):
 
         # Update fails
         await self.sys_dbus.rauc.update()
-        _LOGGER.error("HassOS update fails with: %s", self.sys_dbus.rauc.last_error)
+        _LOGGER.error("HassOS update fails with: %s",
+                      self.sys_dbus.rauc.last_error)
         raise HassOSUpdateError()
 
     async def update_cli(self, version: Optional[str] = None) -> None:
@@ -219,4 +220,5 @@ class HassOS(CoreSysAttributes):
         except DBusError:
             _LOGGER.error("Can't mark booted partition as healty!")
         else:
-            _LOGGER.info("Rauc: %s - %s", self.sys_dbus.rauc.boot_slot, response[1])
+            _LOGGER.info("Rauc: %s - %s", self.sys_dbus.rauc.boot_slot,
+                         response[1])
